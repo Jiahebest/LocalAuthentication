aa6d665d0a4eacf133cf65401641558bfc4da0b1
gdcp <gdcp@m25.local>
2018-05-24 10:56:17 +0800

    createfirst

--- LocalAuthenticationDemo/ViewController.swift
+++ LocalAuthenticationDemo/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
-
+import LocalAuthentication
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let context = LAContext()
+        let result = context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: nil)
+        if result{
+            context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "请使用Touch ID指纹识别", reply: {(success,error) in print(success)}
+            )
+        }
     }
 
     override func didReceiveMemoryWarning() {
